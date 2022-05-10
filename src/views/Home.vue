861d5054d15828c2d688fb7d042d86cfeed0ffd1
Ziyi Xin <ziyixin@169-231-129-74.wireless.ucsb.edu>
2022-05-10 16:04:24 -0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,38 @@
 <script setup>
-let cot = $ref(0)
+let cot = $ref(1);
+let search = $ref("");
+let upper = $computed(() => search.toUpperCase());
+const msg = "Hello, Vue!";
+function print() {
+  console.log(msg);
+}
 
-function inc () {
-  cot++
+function inc() {
+  cot = cot + 2;
 }
 </script>
 
 <template>
   <h1>Home</h1>
   <p>cot: {{ cot }}</p>
-  <button @click="inc">Click Me</button>
+  <input v-model="search" /><br />
+  <p>Your input: {{ search }}</p>
+  <p :class="upper === search ? 'green' : 'red'">Upper Case: {{ upper }}</p>
+
+  <button @click="search = ''">Clear</button><br />
+  <button @click="inc">Click Me</button> <br />
+  <button @click="print">Click Me Again</button>
 </template>
+
+
+<style scoped>
+input {
+  background: #eee;
+}
+.green {
+  color: green;
+}
+.red {
+  color: red;
+}
+</style>
